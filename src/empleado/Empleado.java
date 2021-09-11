a878343628d3399655a47ac332aa12520024e3a5
Yorlady <yorladymontoya@gmail.com>
2021-09-10 19:31:42 -0500

    Se crea menu y metodos get y set

--- src/empleado/Empleado.java
+++ src/empleado/Empleado.java
@@ -1,14 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package empleado;
 
-/**
- *
- * @author yormo
- */
+
 public class Empleado {
+    //Atributos=variables = datos
     
+   private int id;
+   private String nombre;
+   private String cargo;
+   private double horaSemanales;
+   private int salario;
+   
+   //Metodos=accion=funcion especial
+   
+       public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getHoraSemanales() {
+        return horaSemanales;
+    }
+
+    public void setHoraSemanales(double horaSemanales) {
+        
+        
+        if(horasSemanales <0){
+            System.out.println("Formato invalido de hora");
+        }else{
+             this.horaSemanales = horaSemanales;
+        }
+        
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {   
+        this.salario = salario;
+    }
+
+    //constructor
+    public Empleado() {
+    }
+       
+    //Metodos=Acciones=Funciones
+public int calcularSalario(double horas, int valorHora){
+    
+    
+    
+    return 0;
+    
+}
+ 
 }
--- src/evilcorp/EvilCorp.java
+++ src/evilcorp/EvilCorp.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package evilcorp;
 
-/**
- *
- * @author yormo
- */
+import java.util.Scanner;
+
+
 public class EvilCorp {
 
     /**
@@ -16,6 +11,27 @@ public class EvilCorp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        
+        int opcion;
+        do{
+            
+        System.out.println("---------------------------------MENU EMPLEADO---------------------------------------");
+        
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("----1.AGREGAR EMPLEADO.");
+        System.out.println("----2.BUSCAR EMPLEADO.");
+        System.out.println("----3.MOSTRAR EMPLEADO.");
+        System.out.println("----4.CALCULAR SALARIO.");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        
+        }while(opcion!=5);
+        
+      
+        
     }
+    
+    
+    
     
 }
